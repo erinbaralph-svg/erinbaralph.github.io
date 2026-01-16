7aaf2ee644d465af821343a27d5767f3bad57f34
erinbaralph-svg <erinbaralph@gmail.com>
2026-01-16 15:10:34 -0500

    stuff

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,20 +7,40 @@ export const metadata: Metadata = {
   title: "Projects - Erin Ralph",
 };
 
-const projects: ShowcaseData[] = [];
-
-export default function ProjectsPage() {
-  return (
-    <>
-      <Navbar currentPageHref="/projects" />
-      <main className="bg-[#D5D1C8] overflow-y-auto flex flex-col flex-grow">
-        <div className="flex flex-col">
-          {projects.map((e, index) => (
-            <Showcase key={index} headerBackgroundColor={"#075016"} {...e} />
-          ))}
-          <NotFound />
-        </div>
-      </main>
-    </>
-  );
-}
+const projects: ShowcaseData[] = [
+  {
+    title: "Museum Preperator",
+    date: "Summer 2023",
+    location: "Uxbridge Historical Centre",
+    descriptionParagraphs: [
+      "As Museum Preparator at the Uxbridge Historical Centre, I completed extensive site maintenance and preparation work, including painting key exterior structures, adding high-visibility accessibility markings, and maintaining large farm equipment to ensure visitor safety and artifact preservation.",
+    ],
+    images: [
+  
+        src: "/img/programming/children/vbcstmarks.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/summercampuhc.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/zoo2youuhc.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/kidsclub.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/kidsclub2.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/kidsclub3.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/lemonadefoodbank.avif",
+        caption: "",
+      },]
