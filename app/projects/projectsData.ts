c075f0a774c3c0d8daabbacfef93a52566ff6cdd
erinbaralph-svg <erinbaralph@gmail.com>
2026-01-23 13:40:19 -0500

    projects

--- app/projects/projectsData.ts
+++ app/projects/projectsData.ts
@@ -39,5 +39,81 @@ export const projects: ArticleData[] = [
         caption: "",
       },
     ],
+  },{
+    id: "uxbridge-attic",
+    title: "School House Attic Clean Out",
+    date: "Summer 2024",
+    location: "Uxbridge Historical Centre",
+    descriptionParagraphs: [
+      "In the summer of 2024, I worked as a Collections Assistant at the Uxbridge Historical Centre. My main responsibility was a large-scale project involving the cleaning and reorganization of the attic in one of the Centre’s historic buildings. Through this work, I gained hands-on experience in deaccessioning, condition reporting, cataloguing, inventory management, proper storage, and safe disposal practices. I used Excel and museum collections management software to document artifacts and maintain accurate records, while following conservation and safety standards throughout the project.",
+    ],
+    images: [
+      {
+        src: "/img/programming/children/vbcstmarks.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/summercampuhc.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/zoo2youuhc.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/kidsclub.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/kidsclub2.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/kidsclub3.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/lemonadefoodbank.avif",
+        caption: "",
+      },
+    ],
+  },{
+    id: "archives",
+    title: "All Saints' Church Archives",
+    date: "May 2025 - Present",
+    location: "All Saints' Anglican Church, Whitby",
+    descriptionParagraphs: [
+      "Over the past year, I have undertaken a major overhaul project in the archives of All Saints’ Church in Whitby. This work has included archival cleaning and basic conservation, reorganizing and cataloguing materials, accessioning records, and building an entirely new archival database from the ground up. I have also contributed to creating public history displays to make the collection more accessible to the community.",
+    ],
+    images: [
+      {
+        src: "/img/programming/children/vbcstmarks.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/summercampuhc.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/zoo2youuhc.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/kidsclub.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/kidsclub2.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/kidsclub3.avif",
+        caption: "",
+      },
+      {
+        src: "/img/programming/children/lemonadefoodbank.avif",
+        caption: "",
+      },
+    ],
   },
 ];
