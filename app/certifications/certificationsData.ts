bb8eb71a306198107b0773486c06dca01ce622fe
sammypotter <50476978+0spotter0@users.noreply.github.com>
2026-03-02 18:18:16 -0800

    add certs

--- app/certifications/certificationsData.ts
+++ app/certifications/certificationsData.ts
@@ -3,32 +3,32 @@ import { type BadgeData } from "@/components/Badge"
 export const certifications: BadgeData[] = [
   {
     title: "CPR, First Aid, and AED",
-    link: "/ErinRalph.pdf",
+    link: "/cert/CPR.pdf",
     iconSrc: "/img/certifications/red-cross.png",
   },
   {
     title: "Workplace Hazardous Materials Information System (WHMIS)",
-    link: "/ErinRalph.pdf",
+    link: "/cert/WHMIS.pdf",
     iconSrc: "/img/certifications/alert.png",
   },
   {
     title: "Ladder Safety Certification",
-    link: "/ErinRalph.pdf",
+    link: "/cert/LADDER.pdf",
     iconSrc: "/img/certifications/keys.png",
   },
   {
     title: "Vulnerable Sector Screening",
-    link: "/ErinRalph.pdf",
+    link: "/cert/PRC.pdf",
     iconSrc: "/img/certifications/butterfly.png",
   },
   {
     title: "Accessibility Standards Training (AODA)",
-    link: "/ErinRalph.pdf",
+    link: "/cert/AODA.pdf",
     iconSrc: "/img/certifications/tree.png",
   },
   {
     title: "Tourism and Visitor Services Training",
     link: "/ErinRalph.pdf",
-    iconSrc: "/img/certifications/question.png",
+    iconSrc: "/img/certifications/tourism.png",
   },
 ];
