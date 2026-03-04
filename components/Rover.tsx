390a9d1751d3cc9ab5d090ee97fc183f5db080aa
sammypotter <50476978+0spotter0@users.noreply.github.com>
2026-03-03 18:08:54 -0800

    fix: rover bubble pixel border

--- components/Rover.tsx
+++ components/Rover.tsx
@@ -31,8 +31,10 @@ export function Rover({
         isRoverVisible ? "opacity-100" : "opacity-0"
       )}
     >
-      <div className="max-w-[22ch] top-0 left-0 bg-[#FFFFE1] border-2 border-black rounded-xl p-2 px-4 text-sm sm:text-base">
-        {children}
+      <div className="pixel-corners-border bg-black max-w-[22ch] p-[2px] overflow-hidden top-0 left-0 text-sm sm:text-base">
+        <div className="pixel-corners-inner bg-[#FFFFE1] w-full p-2 px-3">
+          {children}
+        </div>
       </div>
       <div className="top-10 left-0">
         <img src="/img/rover.png" className="w-14 sm:w-20 pixelated" />
